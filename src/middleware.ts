47b0a30b99cab717af1cd3460d9423b2dc9cf79e
The-Yearly <theyearlysclub@gmail.com>
2025-07-24 23:25:48 +0530

    misc

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,7 @@
-import axios from "axios";
 import { NextResponse, NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
   const path = req.nextUrl.toString();
-  const currentSeats = await axios.get(
-    process.env.NEXT_PUBLIC_BACKEND_URL + "/seats-left/",
-  );
-  if(currentSeats.data.seat_left<=0 && !path.includes("/payment") && !path.includes("/seats-filled")){
-    return NextResponse.redirect(path+"/seats-filled")
-  }
-  if (path.includes("https://events.amfoss.in/")||path.includes("3000")) {
+  if (path.includes("https://events.amfoss.in/")) {
     return NextResponse.redirect("https://openworkshop.vercel.app/register");
   }
 }
