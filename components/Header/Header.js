68b111b4a9f3ea4f0d184361a08034cdaa95eeea
Nandu Krishna <nandukrishna.mpm@gmail.com>
2025-10-04 12:34:01 +0530

    updated paths for nextjs

---
+++ components/Header/Header.js
@@ -0,0 +1,44 @@
+'use client';
+
+import React from "react";
+import './Header.css';
+import Countdown from "../Countdown/CountdownTimer";
+
+
+function Header() {
+    const eventDate = "2024-10-21";
+
+    const scrollToRegistration = () => {
+        const registrationSection = document.getElementById("registration");
+        if (registrationSection) {
+            registrationSection.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
+
+
+    return (
+        <div className="header">
+            <div className="header-top" >
+                <a href="https://amfoss.in"><img src="/assets/amfoss_logo.png" alt="amfoss logo" className="logo" /></a>
+                <a href="https://www.amrita.edu/campus/amritapuri/"><img src="/assets/amrita_logo.png" alt="amrita logo" className="logo" /></a>
+            </div>
+            <div className="header-main" >
+                <div className="box">
+                </div>
+                <div className="left-content">
+                    <img src="/assets/HF-Horizontal-Color-Light.svg" alt="hacktoberfest logo" className="mainLogo" />
+                    <p className="subtext"> It's that time of year again.<br />HacktoberFest is nearly upon us!</p>
+                    <button className="register-btn" onClick={scrollToRegistration}>REGISTER NOW</button>
+                </div>
+                <div className="right-content">
+                </div>
+
+                <Countdown eventDate={eventDate} />
+            </div>
+
+        </div>
+    )
+}
+
+export default Header;
