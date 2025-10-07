1b490f42eb14a97961fe468362a25e30e0d64412
Nandu Krishna <nandukrishna.mpm@gmail.com>
2025-10-07 23:28:32 +0530

    Fixed ESLint errors and optimized images

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,6 +1,7 @@
 'use client';
 
 import React from "react";
+import Image from 'next/image';
 import './Header.css';
 import Countdown from "../Countdown/CountdownTimer";
 
@@ -20,15 +21,15 @@ function Header() {
     return (
         <div className="header">
             <div className="header-top" >
-                <a href="https://amfoss.in"><img src="/assets/amfoss_logo.png" alt="amfoss logo" className="logo" /></a>
-                <a href="https://www.amrita.edu/campus/amritapuri/"><img src="/assets/amrita_logo.png" alt="amrita logo" className="logo" /></a>
+                <a href="https://amfoss.in"><Image src="/assets/amfoss_logo.png" alt="amfoss logo" className="logo" width={100} height={100} /></a>
+                <a href="https://www.amrita.edu/campus/amritapuri/"><Image src="/assets/amrita_logo.png" alt="amrita logo" className="logo" width={100} height={100} /></a>
             </div>
             <div className="header-main" >
                 <div className="box">
                 </div>
                 <div className="left-content">
-                    <img src="/assets/HF-Horizontal-Color-Light.svg" alt="hacktoberfest logo" className="mainLogo" />
-                    <p className="subtext"> It's that time of year again.<br />HacktoberFest is nearly upon us!</p>
+                    <Image src="/assets/HF-Horizontal-Color-Light.svg" alt="hacktoberfest logo" className="mainLogo" width={400} height={100} />
+                    <p className="subtext"> It&apos;s that time of year again.<br />HacktoberFest is nearly upon us!</p>
                     <button className="register-btn" onClick={scrollToRegistration}>REGISTER NOW</button>
                 </div>
                 <div className="right-content">
